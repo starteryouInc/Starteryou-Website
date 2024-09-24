b91e9e90d32ab174c8b7656b6832d9b2f27562bb
SidAsif <asif050md@gmail.com>
2024-09-24 09:15:07 +0530

    BestJob4 Section

--- Starteryou/src/App.jsx
+++ Starteryou/src/App.jsx
@@ -2,6 +2,7 @@ import BestBuddy from "./components/BestBuddy";
 import BestJob from "./components/BestJob";
 import BestJob2 from "./components/BestJob2";
 import BestJob3 from "./components/BestJob3";
+import BestJob4 from "./components/BestJob4";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -15,6 +16,7 @@ function App() {
         <BestJob />
         <BestJob2 />
         <BestJob3 />
+        <BestJob4 />
       </div>
     </>
   );
