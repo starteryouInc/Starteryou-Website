4b2a893699c971a5844f5b0c85c0198a92dfd7b1
SidAsif <asif050md@gmail.com>
2024-10-02 18:34:57 +0530

    Our Mission_Vision Added

--- Starteryou/src/pages/AboutPage.jsx
+++ Starteryou/src/pages/AboutPage.jsx
@@ -1,11 +1,17 @@
 import BetterFuture from "../components/About/BetterFuture";
 import HeroAbout from "../components/About/HeroAbout";
+import OurMission from "../components/About/OurMission";
+import OurVision from "../components/About/OurVision";
+import Contact from "../components/Landing/Contact";
 
 export default function AboutPage() {
   return (
     <div>
       <HeroAbout />
       <BetterFuture />
+      <OurMission />
+      <OurVision />
+      <Contact />
     </div>
   );
 }
