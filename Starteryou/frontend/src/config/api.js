8a20aea9e6106a65e4a493ce3a8a4bbab6d4265c
RohitKumar710 <bpscrohit@gmail.com>
2024-11-09 00:01:06 -0500

    Updating Api.confg

--- Starteryou/frontend/src/config/api.js
+++ Starteryou/frontend/src/config/api.js
@@ -2,6 +2,7 @@
 export const API_CONFIG = {
   baseURL: "http://54.196.202.145:3000",
   endpoints: {
+    fileByTitle: (title) => `/api/files/download/${title}`,
     fileUpload: "/api/files/upload",
     fileDownload: (title) => `/api/files/download/${title}`,
     fileUpdate: (title) => `/api/files/update/${title}`,
