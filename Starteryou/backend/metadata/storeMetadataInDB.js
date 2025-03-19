00d3f595424bd2b9878837e7785ac34fb169f5f7
Nikshep A Kulli <nikshep.kulli@starteryou.com>
2025-03-19 12:14:35 -0500

    Update storeMetadataInDB.js
    Signed-off-by: Nikshep A Kulli <nikshep.kulli@starteryou.com>

--- Starteryou/backend/metadata/storeMetadataInDB.js
+++ Starteryou/backend/metadata/storeMetadataInDB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const { BaseUser, JobSeeker, Employer } = require("./models"); // Import models
-
+const { BaseUser, JobSeeker, Employer } = require("../models/BaseUserSchema");
+ 
 // Function to store metadata in separate collections
 const storeMetadataInDB = async () => {
   try {
